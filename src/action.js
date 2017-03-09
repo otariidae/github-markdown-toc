b0b504e0d4710f4351a0e33042a27932996514ca
otariidae <otariidae@users.noreply.github.com>
2017-03-10 03:57:45 +0900

    split the code into two files

--- src/action.js
+++ src/action.js
@@ -1,19 +1,22 @@
 const key = require('./action-type.js')
 const { Action } = require('material-flux')
-const checkJSEnabled = require('./check-js-enabled.js')
 const checkPageType = require('./check-page-type.js')
-const headerSelector = 'h1, h2, h3, h4, h5, h6'
+const {
+  fetchReleaseHeader,
+  fetchMarkdownHeader,
+  fetchWikiHeader
+} = require('./get-header-data.js')
 
 class AppAction extends Action {
   async moveToPage () {
     let headers
     const type = checkPageType(location.href)
     if (type === 'release') {
-      headers = this.fetchReleaseHeader()
+      headers = fetchReleaseHeader()
     } else if (type === 'code') {
-      headers = await this.fetchMarkdownHeader()
+      headers = await fetchMarkdownHeader()
     } else if (type === 'wiki') {
-      headers = await this.fetchWikiHeader()
+      headers = await fetchWikiHeader()
     } else {
       headers = []
     }
@@ -26,59 +29,6 @@ class AppAction extends Action {
   }
   toggleNav () {
     this.dispatch(key.TOGGLE_NAV)
-  }
-  fetchReleaseHeader () {
-    const hs = document.querySelectorAll('.release-title')
-    if (!hs) {
-      return []
-    }
-    return Array.from(hs).map(h => {
-      const { href: link } = h.querySelector('a')
-      // all level is 1
-      const tag = 'lv1'
-      const text = h.textContent.trim()
-      return {
-        link,
-        tag,
-        text
-      }
-    })
-  }
-  async fetchMarkdownHeader () {
-    const readme = document.querySelector('.markdown-body')
-    if (!readme) {
-      return []
-    }
-    const hs = readme.querySelectorAll(headerSelector)
-    return await this.getHeaderDataFromMarkdownDOM(hs)
-  }
-  async fetchWikiHeader () {
-    const markdown = document.querySelector('.wiki-body .markdown-body')
-    if (!markdown) {
-      return []
-    }
-    let hs = markdown.querySelectorAll(headerSelector)
-    hs = Array.from(hs).filter(h => {
-      return Boolean(h.textContent.trim())
-    })
-    return await this.getHeaderDataFromMarkdownDOM(hs)
-  }
-  async getHeaderDataFromMarkdownDOM (nodelist) {
-    const isJSEnabled = await checkJSEnabled()
-    const arr = Array.isArray(nodelist) ? nodelist : Array.from(nodelist)
-    return arr.map(h => {
-      let { id, href } = h.querySelector('.anchor')
-      id = `#${id}`
-      href = new URL(href).hash
-      const link = isJSEnabled ? href : id
-      const tag = h.tagName.replace('H', 'lv')
-      const text = h.textContent.trim()
-      return {
-        link,
-        tag,
-        text
-      }
-    })
   }
 }
 
---
+++ src/get-header-data.js
@@ -0,0 +1,65 @@
+const checkJSEnabled = require('./check-js-enabled.js')
+const headerSelector = 'h1, h2, h3, h4, h5, h6'
+
+function fetchReleaseHeader () {
+  const hs = document.querySelectorAll('.release-title')
+  if (!hs) {
+    return []
+  }
+  return Array.from(hs).map(h => {
+    const { href: link } = h.querySelector('a')
+    // all level is 1
+    const tag = 'lv1'
+    const text = h.textContent.trim()
+    return {
+      link,
+      tag,
+      text
+    }
+  })
+}
+
+async function fetchMarkdownHeader () {
+  const readme = document.querySelector('.markdown-body')
+  if (!readme) {
+    return []
+  }
+  const hs = readme.querySelectorAll(headerSelector)
+  return await getHeaderDataFromMarkdownDOM(hs)
+}
+
+async function fetchWikiHeader () {
+  const markdown = document.querySelector('.wiki-body .markdown-body')
+  if (!markdown) {
+    return []
+  }
+  let hs = markdown.querySelectorAll(headerSelector)
+  hs = Array.from(hs).filter(h => {
+    return Boolean(h.textContent.trim())
+  })
+  return await getHeaderDataFromMarkdownDOM(hs)
+}
+
+async function getHeaderDataFromMarkdownDOM (nodelist) {
+  const isJSEnabled = await checkJSEnabled()
+  const arr = Array.isArray(nodelist) ? nodelist : Array.from(nodelist)
+  return arr.map(h => {
+    let { id, href } = h.querySelector('.anchor')
+    id = `#${id}`
+    href = new URL(href).hash
+    const link = isJSEnabled ? href : id
+    const tag = h.tagName.replace('H', 'lv')
+    const text = h.textContent.trim()
+    return {
+      link,
+      tag,
+      text
+    }
+  })
+}
+
+module.exports = {
+  fetchReleaseHeader,
+  fetchMarkdownHeader,
+  fetchWikiHeader
+}
