937c1583baf86426a867370cff8b795227bdadbc
otariidae <otariidae@users.noreply.github.com>
2017-03-13 01:24:46 +0900

    add source map files

--- dist/background.js
+++ dist/background.js
@@ -1,1 +1,2 @@
 chrome.runtime.onMessage.addListener(({type:a},{url:b},c)=>{if('is-js-enabled'===a)return chrome.contentSettings.javascript.get({primaryUrl:b},({setting:d})=>{c('allow'===d)}),!0});
+//# sourceMappingURL=background.js.map
