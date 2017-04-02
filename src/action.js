dcfb5738fe9bc0baf7668168a65b1b49885f230a
otariidae <otariidae@users.noreply.github.com>
2017-04-02 11:10:23 +0900

    use class instead of functions

--- src/action.js
+++ src/action.js
@@ -1,5 +1,6 @@
 import { Action } from '../modules/flux/index.js'
-import fetchHeader from './get-header-data.js'
+// import fetchHeader from './get-header-data.js'
+import GitHubPage from './page.js'
 
 export const key = {
   START_LOADING: Symbol('start-loading'),
@@ -12,7 +13,8 @@ export default class AppAction extends Action {
     this.dispatch(key.START_LOADING)
   }
   async moveToPage () {
-    const headers = await fetchHeader()
+    const page = GitHubPage.createFromUrl(location.href)
+    const headers = await page.getHeaderList()
     const isAvailable = Boolean(headers.length)
     const data = {
       headers,
