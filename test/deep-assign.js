68ac9c9cdfb61ef2f1b3bf3c366eedff5d6ef6da
otariidae <otariidae@users.noreply.github.com>
2017-04-13 19:05:25 +0900

    improve test

--- test/deep-assign.js
+++ test/deep-assign.js
@@ -2,29 +2,43 @@ import test from 'tape'
 import deepAssign from '../modules/deep-assign/index.js'
 
 test('deep-assign', t => {
-  const target = {
-    hoge: 'fuga'
-  }
-  const source = {
-    foo: 'bar',
-    baz: {
-      qux: 'quux'
+  t.test('', t => {
+    const target = {
+      hoge: 'fuga'
     }
-  }
-  const expected = {
-    hoge: 'fuga',
-    foo: 'bar',
-    baz: {
-      qux: 'quux'
+    const source = {
+      foo: 'bar',
+      baz: {
+        qux: 'quux'
+      }
     }
-  }
-  const result = deepAssign(target, source)
+    const expected = {
+      hoge: 'fuga',
+      foo: 'bar',
+      baz: {
+        qux: 'quux'
+      }
+    }
+    const result = deepAssign(target, source)
 
-  t.ok(Object.is(target, result))
-  t.notOk(Object.is(result, expected))
-  t.notOk(Object.is(source.baz, result.baz))
-  t.deepEqual(result, target)
-  t.deepEqual(expected, result)
+    t.ok(Object.is(target, result))
+    t.notOk(Object.is(result, expected))
+    t.notOk(Object.is(source.baz, result.baz))
+    t.deepEqual(result, target)
+    t.deepEqual(expected, result)
+    t.end()
+  })
+
+  t.test('Object.create(null)', t => {
+    const nullProtoObj = Object.create(null)
+    const obj = {
+      foo: 'bar'
+    }
+    const result = deepAssign(nullProtoObj, obj)
+
+    t.equal(Object.getPrototypeOf(result), null)
+    t.end()
+  })
 
   t.test('ignore null', t => {
     t.deepEqual(deepAssign({}, null), {})
