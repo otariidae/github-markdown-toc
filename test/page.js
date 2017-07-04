ee9377c66a789ad1d2ba015b115f793b450a2a91
otariidae <otariidae@users.noreply.github.com>
2017-07-05 02:24:34 +0900

    add fixtures for tests

--- test/page.js
+++ test/page.js
@@ -1,6 +1,8 @@
 import { describe, it as test } from 'kocha'
 import t from 'assert'
 import { URL } from 'url'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 import { JSDOM } from 'jsdom'
 import PageFactory, {
   GitHubPage,
@@ -10,6 +12,11 @@ import PageFactory, {
   UnknownPage
 } from '../src/page.js'
 import { HeaderRoot } from '../src/functions.js'
+
+function fragmentFromFile(path) {
+  const file = readFileSync(join(__dirname, path))
+  return JSDOM.fragment(file.toString())
+}
 
 // shared class
 const { HTMLHeadingElement } = new JSDOM('').window
@@ -26,37 +33,7 @@ describe('UnknownPage', () => {
 })
 
 describe('ReleasePage', () => {
-  const frag = JSDOM.fragment(`
-    <div class="release-timeline">
-      <div class="release label-latest">
-        <div class="release-body commit open">
-          <div class="release-header">
-            <h1 class="release-title">
-              <a href="/example/example-project/releases/tag/v1.0.0">1.0.0</a>
-            </h1>
-          </div>
-        </div>
-      </div>
-      <div class="release">
-        <div class="release-body commit open">
-          <div class="release-header">
-            <h1 class="release-title">
-              <a href="/example/example-project/releases/tag/v0.0.2">0.0.2</a>
-            </h1>
-          </div>
-        </div>
-      </div>
-      <div class="release">
-        <div class="release-body commit open">
-          <div class="release-header">
-            <h1 class="release-title">
-              <a href="/example/example-project/releases/tag/v0.0.1">0.0.1</a>
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  `)
+  const frag = fragmentFromFile('fixtures/release.html')
   const page = new ReleasePage()
 
   test('inheritance', () => {
@@ -71,31 +48,7 @@ describe('ReleasePage', () => {
 })
 
 describe('Code Page', () => {
-  const frag = JSDOM.fragment(`
-    <article class="markdown-body">
-      <h1>
-        <a id="user-content-example-app" class="anchor" href="#example-app">
-        </a>
-        Example App
-      </h1>
-      <h2>
-        <a id="user-content-install" class="anchor" href="#install">
-        </a>
-        Install
-      </h2>
-      <h3>
-        <a id="user-content-manual-install" class="anchor" href="#manual-install">
-        </a>
-        Manual Install
-      </h3>
-      <h2>
-        <a id="user-content-license" class="anchor" href="#license">
-        </a>
-        License
-      </h2>
-      <p>CC0</p>
-    </article>
-  `)
+  const frag = fragmentFromFile('./fixtures/code.html')
   const page = new CodePage()
   const headers = page.getHeaders(frag)
 
@@ -111,36 +64,7 @@ describe('Code Page', () => {
 })
 
 describe('Wiki Page', () => {
-  const frag = JSDOM.fragment(`
-    <div id="wiki-content" class="wiki-content">
-      <div>
-        <div class="wiki-custom-sidebar wiki-writable markdown-body">
-          <h2>
-            <a id="user-content-nodejs-community-wiki" class="anchor" href="#nodejs-community-wiki"></a> Node.js community wiki
-          </h2>
-        </div>
-      </div>
-
-      <div id="wiki-body" class="wiki-body gollum-markdown-content instapaper_body">
-        <div class="markdown-body">
-          <h1>
-            <a id="user-content-breaking-changes-between-v6-and-v7" class="anchor" href="#breaking-changes-between-v6-and-v7"></a>        Breaking changes between v6 and v7
-          </h1>
-          <p></p>
-          <h2>
-            <a id="user-content-by-subsystem" class="anchor" href="#by-subsystem"></a> By Subsystem
-          </h2>
-          <h3>
-            <a id="user-content-buffer" class="anchor" href="#buffer"></a> buffer
-          </h3>
-          <ul><li></li></ul>
-          <h2>
-            <a id="user-content-dependencies" class="anchor" href="#dependencies"></a> Dependencies
-          </h2>
-        </div>
-      </div>
-    </div>
-  `)
+  const frag = fragmentFromFile('./fixtures/wiki.html')
   const page = new WikiPage()
 
   test('inheritance', () => {
