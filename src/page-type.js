472d9c32b1893757b5175972f71b4c74bd03f9ec
otariidae <otariidae@users.noreply.github.com>
2017-07-08 02:17:13 +0900

    use static properties instead of instance properties

--- src/page-type.js
+++ src/page-type.js
@@ -1,52 +1,51 @@
 export default class PageType {
+  static RELEASE = Symbol('release')
+  static CODE = Symbol('code')
+  static WIKI = Symbol('wiki')
+  static UNKNOWN = Symbol('unknown')
   /**
    * @param {string} url
    */
   constructor (url) {
-    /** @public */
-    this.RELEASE = Symbol('release')
-    this.CODE = Symbol('code')
-    this.WIKI = Symbol('wiki')
-    this.UNKNOWN = Symbol('unknown')
     const { pathname } = new URL(url)
     // the first element is empty
     const [, user, project, type, ...paths] = pathname.split('/')
     if (type === 'releases' && paths.length === 0) {
-      this._type = this.RELEASE
+      this._type = PageType.RELEASE
     } else if (
       type === 'blob' ||
       type === 'tree' ||
       (user && project && !type)
     ) {
-      this._type = this.CODE
+      this._type = PageType.CODE
     } else if (type === 'wiki') {
-      this._type = this.WIKI
+      this._type = PageType.WIKI
     } else {
-      this._type = this.UNKNOWN
+      this._type = PageType.UNKNOWN
     }
   }
   /**
    * @returns {boolean}
    */
   isReleasePage () {
-    return this._type === this.RELEASE
+    return this._type === PageType.RELEASE
   }
   /**
    * @returns {boolean}
    */
   isCodePage () {
-    return this._type === this.CODE
+    return this._type === PageType.CODE
   }
   /**
    * @returns {boolean}
    */
   isWikiPage () {
-    return this._type === this.WIKI
+    return this._type === PageType.WIKI
   }
   /**
    * @returns {boolean}
    */
   isUnknownPage () {
-    return this._type === this.UNKNOWN
+    return this._type === PageType.UNKNOWN
   }
 }
