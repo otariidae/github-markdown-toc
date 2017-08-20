20abecbfbe250303543da1c913b1e1d75009070e
otariidae <otariidae@users.noreply.github.com>
2017-08-20 15:47:24 +0900

    refactor(event-emitter): add an internal method

--- modules/event-emitter/index.js
+++ modules/event-emitter/index.js
@@ -9,7 +9,7 @@ export default class EventEmitter {
    * @param {function} handler
    */
   on (type, handler) {
-    if (!this._handlers.has(type)) {
+    if (!this._has(type)) {
       this._handlers.set(type, new Set())
     }
     this._handlers.get(type).add(handler)
@@ -20,7 +20,7 @@ export default class EventEmitter {
    * @returns {boolean}
    */
   off (type, handler) {
-    if (!this._handlers.has(type)) {
+    if (!this._has(type)) {
       return false
     }
     return this._handlers.get(type).delete(handler)
@@ -30,8 +30,15 @@ export default class EventEmitter {
    * @param {*} data
    */
   emit (type, data) {
-    if (this._handlers.has(type)) {
+    if (this._has(type)) {
       this._handlers.get(type).forEach(func => func(data))
     }
+  }
+  /**
+   * @param {string|Symbol} type
+   * @returns {boolean}
+   */
+  _has (type) {
+    return this._handlers.has(type)
   }
 }
