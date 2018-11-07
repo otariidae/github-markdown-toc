229acb9e246307feed3937165a3a6534dfe01ece
otariidae <otariidae@users.noreply.github.com>
2018-11-08 05:37:58 +0900

    deps: update deps

--- src/components/app.js
+++ src/components/app.js
@@ -10,21 +10,22 @@ export default ({
   heading,
   onButtonClick
 }) => html`
-<div
-  class="github-markdown-toc"
-  aria-disabled$="${!isEnabled}"
-  aria-busy$="${isLoading}"
-  data-open$="${isOpen}">
-  <nav id="${navId}" aria-hidden$="${!isOpen}">
-    <noscript>
-      <p class="flash flash-warn">Enable JavaScript</p>
-    </noscript>
-    ${List(heading)}
-  </nav>
-  <button
-    type="button"
-    on-click="${onButtonClick}"
-    class="toggle-btn"
-    aria-controls$="${navId}"
-    disabled?="${!isEnabled}"></button>
-</div>`
+  <div
+    class="github-markdown-toc"
+    aria-disabled$="${!isEnabled}"
+    aria-busy$="${isLoading}"
+    data-open$="${isOpen}"
+  >
+    <nav id="${navId}" aria-hidden$="${!isOpen}">
+      <noscript> <p class="flash flash-warn">Enable JavaScript</p> </noscript>
+      ${List(heading)}
+    </nav>
+    <button
+      type="button"
+      on-click="${onButtonClick}"
+      class="toggle-btn"
+      aria-controls$="${navId}"
+      disabled?="${!isEnabled}"
+    ></button>
+  </div>
+`
--- src/components/tree-list.js
+++ src/components/tree-list.js
@@ -3,22 +3,24 @@ import { html } from 'lit-html/lib/lit-extended'
 const Link = item => {
   if (item.link) {
     return html`
-    <a href="${item.link}">${item.text}</a>
+      <a href="${item.link}">${item.text}</a>
     `
   }
   return item.text
 }
 
 const TreeList = root => html`
-<ol>
-  ${root.sections.map(
+  <ol>
+    ${
+  root.sections.map(
     item => html`
-  <li>
-    ${Link(item)}
-    ${item.sections.length !== 0 ? TreeList(item) : null}
-  </li>
-    `
-  )}
-</ol>`
+          <li>
+            ${Link(item)} ${item.sections.length !== 0 ? TreeList(item) : null}
+          </li>
+        `
+  )
+}
+  </ol>
+`
 
 export default TreeList
