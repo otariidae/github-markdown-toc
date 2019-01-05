6a9a2307e891afa8a6e65a96bf001686bba42d9c
otariidae <otariidae@users.noreply.github.com>
2019-01-06 03:12:40 +0900

    refactor: use sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
+const { resolve } = require('path')
+
 module.exports = {
   map: {
     inline: false,
     annotation: true
   },
   plugins: [
-    require('postcss-nested')(),
     require('postcss-url')({
       url: 'inline',
+      basePath: resolve(__dirname, './src/css'),
       encodeType: 'base64',
     })
   ]
